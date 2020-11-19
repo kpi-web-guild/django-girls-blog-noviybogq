f1f48c8e338bbdadaec8ffeb22f66801d17efe4b
noviybogq <pavlotereshchuk@gmail.com>
2020-11-19 21:48:15 +0200

    Initial commit

---
+++ FirstDjango/hello/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', 'World'],
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+
